8e16ab1e60dac8e56514e7cf5c2a884e0e235910
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-06-25 13:32:52 -0300

    Create 1255.c

---
+++ 1255.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    int n; 
+    char linha[201];
+    int frequencia[26]; 
+    int i, j;
+    int maior_frequencia;
+    
+    
+    scanf("%d", &n);
+    
+   
+    getchar();
+    
+    
+    for (i = 0; i < n; i++) {
+       
+        fgets(linha, sizeof(linha), stdin);
+        
+        
+        for (j = 0; j < 26; j++) {
+            frequencia[j] = 0;
+        }
+        
+        
+        for (j = 0; j < strlen(linha); j++) {
+            
+            if (isalpha(linha[j])) {
+                
+                int indice = tolower(linha[j]) - 'a';
+                frequencia[indice]++;
+            }
+        }
+        
+        
+        maior_frequencia = 0;
+        
+        for (j = 0; j < 26; j++) {
+            if (frequencia[j] > maior_frequencia) {
+                maior_frequencia = frequencia[j];
+            }
+        }
+        
+        
+        for (j = 0; j < 26; j++) {
+            if (frequencia[j] == maior_frequencia && maior_frequencia > 0) {
+                printf("%c", 'a' + j);
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
