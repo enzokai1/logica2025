f95c193bc99d558192b587682a982670f50a5b7d
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-06-25 14:03:16 -0300

    Create 1013.c

---
+++ 1013.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    
+    while (scanf("%d %d %d", &a, &b, &c) == 3) {
+        
+        int maiorAB = (a + b + abs(a - b)) / 2;
+        
+        
+        int maior = (maiorAB + c + abs(maiorAB - c)) / 2;
+        
+        printf("%d eh o maior\n", maior);
+    }
+    
+    return 0;
+}
