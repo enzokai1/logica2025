6a24b58dbe9ede29aeb4f548972aa75eecde4dbe
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-06-25 14:06:21 -0300

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int distancia;
+    
+    while (scanf("%d", &distancia) == 1) {
+        double tempo = distancia / 0.5;
+        
+        printf("%.0lf minutos\n", tempo);
+    }
+    
+    return 0;
+}
