5a777596c241c87e047457741b7b04faaab9e675
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-06-25 13:54:23 -0300

    Create 1011.c

---
+++ 1011.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double R, volume, pi;
+    pi = 3.14159;
+    scanf("%lf", &R);
+    volume = (4.0/3) * pi * R * R * R;
+    printf("VOLUME = %.3f\n", volume);
+    return 0;
+}
