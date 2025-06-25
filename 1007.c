89e71f7f6b8d461432a1989cc0f29424e1c61068
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-06-25 10:34:29 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ int A, B, C, D, diferenca;
+ scanf("%d", &A);
+ scanf("%d", &B);
+ scanf("%d", &C);
+ scanf("%d", &D);
+ diferenca = (A*B-C*D);
+ printf("DIFERENCA = %d\n", diferenca);
+ 
+    return 0;
+}
