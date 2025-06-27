c56cee2da9ae05f22716822310a2b066f91157c9
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-06-27 11:32:30 -0300

    Create 1065.c

---
+++ 1065.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   int pares, numeros;
+   pares = 0;
+   for(int i = 0; i < 5; i++){
+    scanf("%d", &numeros);
+    if(numeros % 2 == 0)
+    ++pares;
+   }
+   printf("%d valores pares\n", pares);
+return 0;
+}
