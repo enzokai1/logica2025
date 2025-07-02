0b5dbeceaa6a4b2259ea5e46915dc737e08514fd
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:49:58 -0300

    Update 1065.c

--- 1065.c
+++ 1065.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main()
 {
-   int pares, numeros;
-   pares = 0;
-   for(int i = 0; i < 5; i++){
-    scanf("%d", &numeros);
-    if(numeros % 2 == 0)
-    ++pares;
-   }
-   printf("%d valores pares\n", pares);
-return 0;
+    int pares, numeros;
+    pares = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &numeros);
+        if (numeros % 2 == 0)
+            ++pares;
+    }
+    printf("%d valores pares\n", pares);
+    return 0;
 }
