bdca07ad2561e42630f387b0801f9e5cf4eaaf78
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:43:40 -0300

    Update 1013.c

--- 1013.c
+++ 1013.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     int a, b, c;
-    
-    while (scanf("%d %d %d", &a, &b, &c) == 3) {
-        
+
+    while (scanf("%d %d %d", &a, &b, &c) == 3)
+    {
+
         int maiorAB = (a + b + abs(a - b)) / 2;
-        
-        
+
         int maior = (maiorAB + c + abs(maiorAB - c)) / 2;
-        
+
         printf("%d eh o maior\n", maior);
     }
-    
+
     return 0;
 }
