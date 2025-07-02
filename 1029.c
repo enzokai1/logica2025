01c2836ea8442d412a0f6554eda67c21233099af
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:47:17 -0300

    Update 1029.c

--- 1029.c
+++ 1029.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 
-int calls; 
+int calls;
 
-int fibonacci(int n) {
-    calls++; 
-    
-    if (n == 0) {
+int fibonacci(int n)
+{
+    calls++;
+
+    if (n == 0)
+    {
         return 0;
     }
-    if (n == 1) {
+    if (n == 1)
+    {
         return 1;
     }
-    
-    return fibonacci(n-1) + fibonacci(n-2);
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main() {
+int main()
+{
     int N;
     scanf("%d", &N);
-    
-    for (int i = 0; i < N; i++) {
+
+    for (int i = 0; i < N; i++)
+    {
         int X;
         scanf("%d", &X);
-        
-        calls = 0; 
+
+        calls = 0;
         int result = fibonacci(X);
-        
-        
+
         calls = calls - 1;
-        
+
         printf("fib(%d) = %d calls = %d\n", X, calls, result);
     }
-    
+
     return 0;
 }
