7b06b2a629e0f4cf702968e0585270c7a6692137
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:44:02 -0300

    Update 1014.c

--- 1014.c
+++ 1014.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int distancia;
     double combustivel;
-    
-    while (scanf("%d %lf", &distancia, &combustivel) == 2) {
+
+    while (scanf("%d %lf", &distancia, &combustivel) == 2)
+    {
         double consumo = distancia / combustivel;
         printf("%.3lf km/l\n", consumo);
     }
-    
+
     return 0;
 }
