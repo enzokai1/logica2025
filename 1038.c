13eabf7299421d9c89f4408ef79e446bcf3c3c70
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:48:39 -0300

    Update 1038.c

--- 1038.c
+++ 1038.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int codigo, quantidade;
     double preco, total;
-    
+
     scanf("%d %d", &codigo, &quantidade);
-    
-    if(codigo == 1) {
+
+    if (codigo == 1)
+    {
         preco = 4.00;
-    } else if(codigo == 2) {
+    }
+    else if (codigo == 2)
+    {
         preco = 4.50;
-    } else if(codigo == 3) {
+    }
+    else if (codigo == 3)
+    {
         preco = 5.00;
-    } else if(codigo == 4) {
+    }
+    else if (codigo == 4)
+    {
         preco = 2.00;
-    } else if(codigo == 5) {
+    }
+    else if (codigo == 5)
+    {
         preco = 1.50;
     }
-    
+
     total = preco * quantidade;
-    
+
     printf("Total: R$ %.2f\n", total);
-    
+
     return 0;
 }
