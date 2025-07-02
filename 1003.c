745c7ede94307edc33a58444edf0f0b92ac00d70
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:39:27 -0300

    Update 1003.c

--- 1003.c
+++ 1003.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int main() {
-   int A, B, SOMA;
-   scanf("%d",&A);
-   scanf("%d", &B);
+int main()
+{
+    int A, B, SOMA;
+    scanf("%d", &A);
+    scanf("%d", &B);
 
-   SOMA=A+B;
-   printf("SOMA = %d\n", SOMA);
-
+    SOMA = A + B;
+    printf("SOMA = %d\n", SOMA);
 
     return 0;
-
-
 }
