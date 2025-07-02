7582cecbaae3a2a2f7f435877e26f0c59ba94d68
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:42:08 -0300

    Update 1009.c

--- 1009.c
+++ 1009.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
- 
-int main() {
+
+int main()
+{
     char nome[10];
     double salario, comissao, vendastotal, total;
     scanf("%s", nome);
     scanf("%lf", &salario);
     scanf("%lf", &vendastotal);
-    comissao = vendastotal*0.15;
+    comissao = vendastotal * 0.15;
     total = (salario + comissao);
     printf("TOTAL = R$ %.2lf\n", total);
- 
+
     return 0;
 }
