517cc7b7811275d3a015649cf14afc58f90ee67a
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:43:21 -0300

    Update 1012.c

--- 1012.c
+++ 1012.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     double A, B, C;
     double pi = 3.14159;
-    
-    while (scanf("%lf %lf %lf", &A, &B, &C) == 3) {
-        
+
+    while (scanf("%lf %lf %lf", &A, &B, &C) == 3)
+    {
+
         double triangulo = (A * C) / 2.0;
-        
-        
+
         double circulo = pi * C * C;
-        
-        
+
         double trapezio = ((A + B) * C) / 2.0;
-        
-        
+
         double quadrado = B * B;
-        
-        
+
         double retangulo = A * B;
-        
+
         printf("TRIANGULO: %.3lf\n", triangulo);
         printf("CIRCULO: %.3lf\n", circulo);
         printf("TRAPEZIO: %.3lf\n", trapezio);
         printf("QUADRADO: %.3lf\n", quadrado);
         printf("RETANGULO: %.3lf\n", retangulo);
     }
-    
+
     return 0;
 }
