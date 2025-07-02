62dea48167a5ff62f872f83d95ba0356f4990a21
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:40:52 -0300

    Update 1006.c

--- 1006.c
+++ 1006.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
- 
-int main() {
+
+int main()
+{
     double A, B, C, media;
     scanf("%lf", &A);
     scanf("%lf", &B);
     scanf("%lf", &C);
-    media = (A*2+B*3+C*5)/10;
+    media = (A * 2 + B * 3 + C * 5) / 10;
     printf("MEDIA = %.1lf\n", media);
     return 0;
 }
