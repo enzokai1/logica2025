8efbe9e9d37107399b61e6b78d76b701a903ea81
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:52:12 -0300

    Update 1253.c

--- 1253.c
+++ 1253.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int n;
     char texto_cifrado[51];
     int deslocamento;
-    
-    
+
     scanf("%d", &n);
-    
-   
-    for (int caso = 0; caso < n; caso++) {
-        
+
+    for (int caso = 0; caso < n; caso++)
+    {
+
         scanf("%s", texto_cifrado);
-        
-        
+
         scanf("%d", &deslocamento);
-        
-        
+
         int tamanho = strlen(texto_cifrado);
-        
-        
-        for (int i = 0; i < tamanho; i++) {
+
+        for (int i = 0; i < tamanho; i++)
+        {
             char letra_atual = texto_cifrado[i];
             char letra_decodificada;
-            
-            
+
             letra_decodificada = letra_atual - deslocamento;
-            
-            
-            if (letra_decodificada < 'A') {
-                
+
+            if (letra_decodificada < 'A')
+            {
+
                 letra_decodificada = letra_decodificada + 26;
             }
-            
-            
+
             texto_cifrado[i] = letra_decodificada;
         }
-        
-        
+
         printf("%s\n", texto_cifrado);
     }
-    
+
     return 0;
 }
