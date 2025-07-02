cdfdf237c926cf5baa52318c02ce80bdfc79604b
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:50:48 -0300

    Update 1070.c

--- 1070.c
+++ 1070.c
@@ -3,19 +3,19 @@
 int main()
 {
     int X;
-    
+
     scanf("%d", &X);
-    
-   
-    if (X % 2 == 0) {
+
+    if (X % 2 == 0)
+    {
         X += 1;
     }
-    
-    
-    for (int i = 0; i < 6; i++) {
+
+    for (int i = 0; i < 6; i++)
+    {
         printf("%d\n", X);
-        X += 2; 
+        X += 2;
     }
-    
+
     return 0;
 }
