7edc50c89abe11961f9eddbb2f77c2f9dc04d24a
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:49:41 -0300

    Update 1060.c

--- 1060.c
+++ 1060.c
@@ -4,13 +4,13 @@ int main()
     double numeros;
     int positivos;
     positivos = 0;
-    for (int i = 0; i < 6; i++){
+    for (int i = 0; i < 6; i++)
+    {
         scanf("%lf", &numeros);
         if (numeros > 0)
-        ++positivos;
+            ++positivos;
     }
     printf("%d valores positivos\n", positivos);
 
-
-return 0;
+    return 0;
 }
