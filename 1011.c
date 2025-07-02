718b15c91ad3b3adb7eb26b041320135fbe4fdf4
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:42:54 -0300

    Update 1011.c

--- 1011.c
+++ 1011.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main()
+{
     double R, volume, pi;
     pi = 3.14159;
     scanf("%lf", &R);
-    volume = (4.0/3) * pi * R * R * R;
+    volume = (4.0 / 3) * pi * R * R * R;
     printf("VOLUME = %.3f\n", volume);
     return 0;
 }
