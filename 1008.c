fbd2354178b42eea67b162455a7852832de4441e
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:41:40 -0300

    Update 1008.c

--- 1008.c
+++ 1008.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
- 
-int main() {
+
+int main()
+{
     int nfuncionario;
     float vporhora, nhoras, salario;
-scanf("%d", &nfuncionario);
-scanf("%f", &nhoras);
-scanf("%f", &vporhora);
-salario = (nhoras*vporhora);
-printf("NUMBER = %d\nSALARY = U$ %.2f\n", nfuncionario, salario);
+    scanf("%d", &nfuncionario);
+    scanf("%f", &nhoras);
+    scanf("%f", &vporhora);
+    salario = (nhoras * vporhora);
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", nfuncionario, salario);
 
     return 0;
 }
