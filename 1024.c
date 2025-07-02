c46031f7539ce3f05ba9ad3fa6053decf564fbe6
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:46:51 -0300

    Update 1024.c

--- 1024.c
+++ 1024.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int n;
     char linha[1001];
-    
-  
+
     scanf("%d", &n);
-    getchar(); 
-    
-    
-    for (int caso = 0; caso < n; caso++) {
-        
+    getchar();
+
+    for (int caso = 0; caso < n; caso++)
+    {
+
         fgets(linha, sizeof(linha), stdin);
-        
-       
+
         int tamanho = strlen(linha);
-        if (tamanho > 0 && linha[tamanho - 1] == '\n') {
+        if (tamanho > 0 && linha[tamanho - 1] == '\n')
+        {
             linha[tamanho - 1] = '\0';
             tamanho--;
         }
-        
-        
-        for (int i = 0; i < tamanho; i++) {
-            if ((linha[i] >= 'A' && linha[i] <= 'Z') || (linha[i] >= 'a' && linha[i] <= 'z')) {
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            if ((linha[i] >= 'A' && linha[i] <= 'Z') || (linha[i] >= 'a' && linha[i] <= 'z'))
+            {
                 linha[i] = linha[i] + 3;
             }
         }
-        
-        
-        for (int i = 0; i < tamanho / 2; i++) {
+
+        for (int i = 0; i < tamanho / 2; i++)
+        {
             char temp = linha[i];
             linha[i] = linha[tamanho - 1 - i];
             linha[tamanho - 1 - i] = temp;
         }
-        
-        
+
         int metade = tamanho / 2;
-        for (int i = metade; i < tamanho; i++) {
+        for (int i = metade; i < tamanho; i++)
+        {
             linha[i] = linha[i] - 1;
         }
-        
-        
+
         printf("%s\n", linha);
     }
-    
+
     return 0;
 }
