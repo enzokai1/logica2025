9bbc434ae65ac6198c9f674111ed0552dab1b2d8
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:47:34 -0300

    Update 1035.c

--- 1035.c
+++ 1035.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int A, B, C, D;
-    
+
     scanf("%d %d %d %d", &A, &B, &C, &D);
-    
-    if(B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) {
+
+    if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0)
+    {
         printf("Valores aceitos\n");
-    } else {
+    }
+    else
+    {
         printf("Valores nao aceitos\n");
     }
-    
+
     return 0;
 }
