d16ae222ea10891da9b50ab506667d8bd20c4734
enzokai1 <enzo.massahiro.kai@gmail.com>
2025-07-02 19:49:23 -0300

    Update 1059.c

--- 1059.c
+++ 1059.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-   for(int par = 2; par <=100; par +=2){
-    printf("%d\n", par);
-   }
-    
+    for (int par = 2; par <= 100; par += 2)
+    {
+        printf("%d\n", par);
+    }
 
     return 0;
 }
